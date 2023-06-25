c1af77531a070a46bb7ffa53657ed02c40037a4a
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2023-06-25 03:13:10 +0200

    Perceptron implemented

---
+++ classification/models/perceptron.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class Perceptron:
+    def __init__(self):
+        self.theta = None
+        self.theta_0 = None
+
+    def fit(self, X_train: np.ndarray, y_train: np.array, T: int):
+        """
+        Train the perceptron on some data
+
+        Parameters:
+            X_train (np.ndarray): Features (2d array of floats)
+            y_train (np.array): Target values (2d array of ints)
+            T (int): Number of iterations (how many times should the algorithm evaluate all the datapoints)
+
+        Returns:
+            None: it updates the class fields
+        """
+        # dimension of theta should be equal to the dimension of feature vectors in X
+        self.theta = np.zeros(X_train.size[1])
+        self.theta_0 = 0
+
+        n = X_train.shape[0] # Number of datapoints
+
+        for t in range(T):
+            for i in range(n):
+                # the condition below is satisfied iff the prediction of the current perceptron state and the actual value mismatch
+                if y_train[i] * (np.dot(self.theta, X_train[i]) + self.theta_0) <= 0:
+                    self.theta += y_train[i]*X_train[i]
+                    self.theta_0 += y_train[i]
+
+    def predict(self, x: np.array) -> int:
+        """
+        Predict the class of a given datapoint x
+
+        Parameters:
+            x (np.array): a new datapoint
+
+        Returns:
+            y (int): either 1 or -1 for binary classification problem
+        """
+        return 1 if np.dot(self.theta, x) + self.theta_0 > 0 else -1
