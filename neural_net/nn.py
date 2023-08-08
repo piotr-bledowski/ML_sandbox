469dc0faf2f5df53faf05315d0b149408747ea99
Piotr <piotr.bledowski.77@gmail.com>
2023-08-08 02:38:41 +0200

    Added update method to Neuron

--- neural_net/nn.py
+++ neural_net/nn.py
@@ -3,7 +3,7 @@ from typing import Callable, Any
 import numpy as np
 
 
-class Neuron():
+class Neuron:
     def __init__(self, weights: np.array[np.float64], bias: np.float64, activation: Callable[[Any], Any]):
         self.weights = weights
         self.bias = bias
@@ -12,7 +12,11 @@ class Neuron():
     def __call__(self, x: np.array) -> np.float64:
         return self.activation(np.dot(self.weights, x) + self.bias)
 
+    def update(self, w: np.array[np.float64], b: np.float64):
+        self.weights = w
+        self.bias = b
 
-class NeuralNetwork():
+
+class NeuralNetwork:
     pass
 
