e3ecc7a5a4eb575b4541e11b191a5896968e4ba1
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2023-08-12 16:08:16 +0200

    added __call__ and update methods to neuron

--- neural_net/nn.py
+++ neural_net/nn.py
@@ -3,9 +3,17 @@ from typing import Callable, Any
 
 
 class Neuron():
-    def __init__(self, weights: np.array[np.float64], activation: Callable[[Any], Any]):
+    def __init__(self, weights: np.array[np.float64], bias: np.float64, activation: Callable[[Any], Any]):
         self.weights = weights
+        self.bias = bias
         self.activation = activation
+
+    def __call__(self, x: np.array) -> np.float64:
+        return self.activation(np.dot(self.weights, x) + self.bias)
+
+    def update(self, w: np.array[np.float64], b: np.float64):
+        self.weights = w
+        self.bias = b
 
 
 class NeuralNetwork():
