Fixed cost derivatives return type

main.py
@@ -11,6 +11,11 @@ import numpy as np
 from classification.models.perceptron import Perceptron
 from plotting import plotBinaryPoints, plotLine
 from neural_net.nn import NeuralNetwork
+from neural_net.layer import FullyConnectedLayer, ActivationLayer
+from neural_net.helpers.activation import *
+from neural_net.helpers.activation_derivatives import *
+from neural_net.helpers.cost import *
+from neural_net.helpers.cost_derivatives import *
 
 from keras.utils import to_categorical
 
@@ -37,11 +42,37 @@ from keras.utils import to_categorical
 # plt.show()
 
 
-train_data = pd.read_csv('mnist_test.csv')
+train_data = pd.read_csv('mnist_train.csv')
 
-X_train = train_data.loc[:, train_data.columns != 'label']
-y_train = to_categorical(train_data['label'])
+X_train = train_data.loc[:, train_data.columns != 'label'].to_numpy(dtype=np.float64)
+X_train = X_train[:1000]
+# print(X_train.dtype)
+X_train /= 255.0  # normalize
+y_train = np.array(to_categorical(train_data['label']))
 
-print(X_train)
-print(y_train)
+X_train = np.expand_dims(X_train, axis=1)
 
+print(X_train.shape)
+
+# print(X_train)
+# print(y_train)
+
+model = NeuralNetwork(layers=[
+        FullyConnectedLayer(784, 200),
+        ActivationLayer(sigmoid, d_sigmoid),
+        FullyConnectedLayer(200, 100),
+        ActivationLayer(sigmoid, d_sigmoid),
+        FullyConnectedLayer(100, 50),
+        ActivationLayer(sigmoid, d_sigmoid),
+        FullyConnectedLayer(50, 10),
+        ActivationLayer(sigmoid, d_sigmoid)
+    ])
+
+model.fit(X_train, y_train, n_epochs=1000, algorithm='mbgd', batch_size=100, learning_rate=0.1)
+
+test_data = pd.read_csv('mnist_test.csv')
+
+X_test = test_data.loc[:, train_data.columns != 'label']
+y_test = to_categorical(test_data['label'])
+
+predictions = []

neural_net/helpers/cost.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def MSE(y_pred: np.array, y_actual: np.array) -> np.float64:
     y = y_pred - y_actual
-    return np.sum(np.dot(y.T, y)) / len(y)
+    return np.mean(np.power(y, 2))
 
 def multiclassCrossEntropy(y_pred: np.array, y_actual: np.array) -> np.float64:
     return np.sum(-y_actual * np.log(y_pred))

neural_net/layer.py
@@ -49,7 +49,7 @@ class FullyConnectedLayer(Layer):
         self.output = np.dot(x, self.weights) + self.bias
         return self.output
 
-    def backward_pass(self, output_error: np.float64, learning_rate: np.float64):
+    def backward_pass(self, output_error: np.array, learning_rate: np.float64):
         """
         Computes dL/dA_l-1 given dL/dZ_l as per MIT OCW 6.036 notation
 

neural_net/nn.py
@@ -25,7 +25,7 @@ import random
 
 
 class NeuralNetwork:
-    def __init__(self, layers: list[type[Layer]] = [], loss: Callable[[Any, Any], Any] = MSE, d_loss: Callable[[Any, Any], Any] = d_MSE):
+    def __init__(self, layers: list[type[Layer]], loss: Callable[[Any, Any], Any] = MSE, d_loss: Callable[[Any, Any], Any] = d_MSE):
         """
         Sequential Neural Net constructor
 
@@ -80,10 +80,10 @@ class NeuralNetwork:
                     output = layer.forward_pass(output)
 
                 # summing epoch error with each datapoint
-                epoch_error += self.loss(train_y[j], output)
+                epoch_error += self.loss(output, train_y[j])
 
                 # Error back propagation - computing partial derivatives corresponding to each layer
-                error = self.d_loss(train_y[j], output)
+                error = self.d_loss(output, train_y[j])
                 for layer in reversed(self.layers):
                     error = layer.backward_pass(error, learning_rate)
 
@@ -92,6 +92,7 @@ class NeuralNetwork:
         self.training_errors = epoch_errors
 
     def miniBatchGradientDescent(self, train_X: np.ndarray, train_y: np.ndarray, n_epochs: int, batch_size: int,
+                                 learning_rate: float = 0.001,
                                  adaptive_step_size_method: str = '',
                                  regularization_method: str = ''):
         """
@@ -109,9 +110,10 @@ class NeuralNetwork:
         n_samples = len(train_X)
 
         for i in range(n_epochs):
-            indices = np.random.choice(n_samples, batch_size)
-            self.batchGradientDescent(train_X[indices], train_y[indices], n_epochs=1)
+            indices = np.random.randint(0, n_samples, size=batch_size)
+            self.batchGradientDescent(train_X[indices], train_y[indices], n_epochs=1, learning_rate=learning_rate)
             epoch_errors.append(self.training_errors[0])
+            print(f'Epoch {i + 1} training error: {self.training_errors[0]}')
 
         self.training_errors = epoch_errors
 
@@ -155,7 +157,12 @@ class NeuralNetwork:
             regularization_method: str = '',
             batch_size: int = 1,
             learning_rate: float = 0.001):
-        pass
+        if algorithm == 'bgd':
+            self.batchGradientDescent(train_X, train_y, n_epochs=n_epochs, learning_rate=learning_rate)
+        elif algorithm == 'mbgd':
+            self.miniBatchGradientDescent(train_X, train_y, n_epochs=n_epochs, learning_rate=learning_rate, batch_size=batch_size)
+        elif algorithm == 'sgd':
+            self.stochasticGradientDescent(train_X, train_y, n_epochs=n_epochs)
 
     def predict(self, x: np.array) -> np.array:
         predictions = []