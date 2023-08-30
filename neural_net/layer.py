85d7f9f01f774e95187e44d38006827b24fc00ed
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2023-08-31 01:56:21 +0200

    Implemented adadelta in Layer classes

--- neural_net/layer.py
+++ neural_net/layer.py
@@ -20,6 +20,9 @@ class Layer:
         """
         pass
 
+    def backwardPassAdadelta(self, output_error: np.float64, learning_rate: np.float64, gamma: np.float64 = 0.9, epsilon: np.float64 = 0.001):
+        pass
+
 
 class FullyConnectedLayer(Layer):
     def __init__(self, m: int, n: int):
@@ -34,6 +37,8 @@ class FullyConnectedLayer(Layer):
         self.bias = np.random.rand(1, n) - 0.5  # n biases for n neurons
         self.weights_momentum = 0
         self.bias_momentum = 0
+        self.weights_adadelta = 0
+        self.bias_adadelta = 0
 
     def forwardPass(self, x: np.array) -> np.array:
         """
@@ -97,6 +102,31 @@ class FullyConnectedLayer(Layer):
 
         return input_error
 
+    def backwardPassAdadelta(self, output_error: np.float64, learning_rate: np.float64, gamma: np.float64 = 0.9, epsilon: np.float64 = 0.001):
+        """
+        Computes dL/dA_l-1 given dL/dZ_l as per MIT OCW 6.036 notation
+
+        Parameters:
+            output_error (np.float64): output error calculated by the following layer
+            learning_rate (np.float64)
+            gamma (np.float64): momentum constant
+
+        Returns:
+            input_error (np.float64): output error of the previous layer
+        """
+        input_error = np.dot(output_error, self.weights.T)
+        weights_error = np.dot(self.input.T, output_error)
+
+        # Exponential Moving Average implementation for both weights and bias
+
+        self.weights_adadelta = gamma * self.weights_adadelta + (1 - gamma) * weights_error**2
+        self.bias_adadelta = gamma * self.bias_adadelta + (1 - gamma) * output_error**2
+
+        self.weights -= learning_rate / np.sqrt(self.weights_adadelta + epsilon) * weights_error
+        self.bias -= learning_rate / np.sqrt(self.bias_adadelta + epsilon) * output_error
+
+        return input_error
+
 
 class ActivationLayer(Layer):
     def __init__(self, activation: Callable[[Any], Any], activation_derivative: Callable[[Any], Any]):
@@ -108,7 +138,7 @@ class ActivationLayer(Layer):
         self.output = self.activation(x)
         return self.output
 
-    def backwardPass(self, output_error: np.float64, learning_rate: np.float64):
+    def backwardPass(self, output_error: np.float64, learning_rate: np.float64 = 1):
         """
         Computes dL/dZ_l given dL/dA_l as per MIT OCW 6.036 notation
 
@@ -122,4 +152,7 @@ class ActivationLayer(Layer):
         return self.d_activation(self.input) * output_error
 
     def backwardPassMomentum(self, output_error: np.array, learning_rate: np.float64, gamma: np.float64 = 0.9):
+        return self.backwardPass(output_error, learning_rate)
+
+    def backwardPassAdadelta(self, output_error: np.float64, learning_rate: np.float64, gamma: np.float64 = 0.9, epsilon: np.float64 = 0.001):
         return self.backwardPass(output_error, learning_rate)
